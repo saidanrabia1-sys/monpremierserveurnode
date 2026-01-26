cf6861a7a3b92ecb613657fb783dbd5ab2aa4476
unknown <saidanrabia1@gmail.com>
2026-01-26 16:22:11 +0300

    rénitialisation du code

--- serveur.js
+++ serveur.js
@@ -9,8 +9,10 @@ const http = require("http");
 // J'importe l'application express app.js
 const app = require('./app');
 
+const numPort = 3003;
+
 // Je configure le numéro du port utilisé dans le serveur
-app.set('port', 3000);
+app.set('port', numPort);
 
 // Maintenant, je crée mon serveur en utilisant le package HTTP
 
@@ -24,8 +26,8 @@ const server = http.createServer(app);
 const date = new Date();
 
 // Je précise l'adresse et le port du serveur
-server.listen(3000, () => {
-    console.log("Le serveur tourne sur le port 3000.");
+server.listen(numPort, () => {
+    console.log("Le serveur tourne sur le port", numPort);
     console.log( date.toLocaleDateString());
     console.log(date.toLocaleTimeString());
 });

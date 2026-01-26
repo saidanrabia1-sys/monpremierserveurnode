a35d3bf10d839533c9f868ce4f13bdc78c8a3cae
unknown <saidanrabia1@gmail.com>
2026-01-26 11:03:48 +0300

    Integrer les middleware dans l'app.js

--- app.js
+++ app.js
@@ -7,9 +7,24 @@ const express = require('express');
 const app = express();
 
 
-app.use((req,res) => {
-    res.end("Bonjour, je suis le serveur!")
+app.use((req,res,next) => {
+    console.log("Je suis un middleware dans le serveur");
+    next();
 });
+
+/* Je suis un Milddeware 2 pou retouener la réponse au client lorsqu'il fait la 
+reqêute localhost:3000. La réponse est au format JSON */
+app.use((req,res) => {
+    res.json({ message: "Vous êtes bien sur le serveur Ndovoo!"});
+  
+});
+
+// J'utilise un Middleware 3 pour afficher le message dans la console 
+app.use((req,res,next) => {
+    console.log("Bonjour, je suis dans le serveur!");
+
+});
+
 
 
 /**
